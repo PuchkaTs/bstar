0477bb552c3598acafe1a6050a8cc7c30eb34ff0
unknown <ncst.puje@gmail.com>
2016-06-21 18:30:11 +0900

    deleted files

--- app/PlaceType.php
+++ app/PlaceType.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PlaceType extends Model
 {
-    protected $table = 'placetypes';
+    protected $table = 'placeTypes';
 
     /**
      * The attributes that are mass assignable.
