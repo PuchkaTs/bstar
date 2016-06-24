0f1351cfd72585f6a70379514d969d6114025978
unknown <ncst.puje@gmail.com>
2016-06-24 13:52:18 +0900

    added chat

--- resources/views/layouts/default_min.blade.php
+++ resources/views/layouts/default_min.blade.php
@@ -21,6 +21,15 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <!--Start of Zopim Live Chat Script-->
+    <script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+    $.src="//v2.zopim.com/?40sqRWFK7giblbbpciqTlMLwjt99yN2C";z.t=+new Date;$.
+    type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+    </script>
+    <!--End of Zopim Live Chat Script-->    
 </head>
 <body>
 
