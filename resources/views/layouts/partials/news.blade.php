dcd3a4a91b727d1f14cdd52e904315a9e5a194a7
unknown <ncst.puje@gmail.com>
2016-07-24 13:29:17 +0900

    news margin added

--- resources/views/layouts/partials/news.blade.php
+++ resources/views/layouts/partials/news.blade.php
@@ -3,31 +3,38 @@
 <h1>Мэдээлэл & Зөвлөгөө </h1>
 	<div class="news_section row">
 
-		<section class="col-md-6" style="height:100%;">
+		<section class="col-md-12" style="height:100%;">
 			@foreach($news->getLatestNews() as $anews)
 				@if($news->getLatestId() == $anews->id)
-                <article class="col-md-12">
-                    <a href="{{ route('news_show_path', $anews->id) }}" >{!! Html::image("assets/news/thumbs/$anews->photo", null, ['class'=>'width100']) !!}</a>
-                    <figcaption>
-                        <h5>{!! link_to_route('news_show_path', $anews->title, $anews->id)!!}</h5>
-                    </figcaption>
-                </article>
+				<div class="row">
+					<div class="col-md-6">
+	                <article style="margin-top:0px; min-height:295px;">
+	                    <a href="{{ route('news_show_path', $anews->id) }}" >{!! Html::image("assets/news/thumbs/$anews->photo", null, ['class'=>'width100']) !!}</a>
+	                    <figcaption>
+	                        <h5>{!! link_to_route('news_show_path', $anews->title, $anews->id)!!}</h5>
+	                    </figcaption>
+	                </article>
+	                </div>
+					<section class="col-md-6">
+						<!-- 16:9 aspect ratio -->
+						<div class="embed-responsive embed-responsive-16by9">
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/uXATjvr_rl4" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</section>  
+				</div>              
 				@else
-                <article class="col-md-6 col-sm-6">
-                    <a href="{{ route('news_show_path', $anews->id) }}" >{!! Html::image("assets/news/thumbs/$anews->photo", null, ['class'=>'width100']) !!}</a>
-                    <figcaption>
-                        <h5>{!! link_to_route('news_show_path', $anews->title, $anews->id)!!}</h5>
-                    </figcaption>
-                </article>
+				<div class="col-md-3 col-sm-6">
+	                <article>
+		                    <a href="{{ route('news_show_path', $anews->id) }}" >{!! Html::image("assets/news/thumbs/$anews->photo", null, ['class'=>'width100']) !!}</a>
+		                    <figcaption>
+		                        <h5>{!! link_to_route('news_show_path', $anews->title, $anews->id)!!}</h5>
+		                    </figcaption>
+	                </article>
+                </div>
                 @endif
 			@endforeach		
 		</section>
-		<section class="col-md-6">
-			<!-- 16:9 aspect ratio -->
-			<div class="embed-responsive embed-responsive-16by9">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/uXATjvr_rl4" frameborder="0" allowfullscreen></iframe>
-			</div>
-		</section>		
+		
 	</div>
 </div>
 <div class="placeholder100"></div>
