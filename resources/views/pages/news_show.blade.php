21bd56a34624503b8338ff9aa49b29eb64542e3e
unknown <ncst.puje@gmail.com>
2016-07-26 12:24:53 +0900

    news refixed

--- resources/views/pages/news_show.blade.php
+++ resources/views/pages/news_show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts/default_min')
 
-@section('body')
+@section('content')
     <div class="placeholder100 row" style="margin-top: 50px;">
-        <div class="col-md-8 col-lg-6 col-lg-offset-2 gheader">
+        <div class="col-md-12 gheader">
             <header>
                 <h3>{{ $anews->title }}</h3>
 
@@ -12,11 +12,11 @@
     </div>
 <div class="row" id="projects" style="min-height: 500px">
     <div>
-            <div class="col-md-12 col-lg-10 col-lg-offset-1" id="project_by_id">
+            <div class="col-md-12" id="project_by_id">
                     <article>
                         <div>{!! Html::image("assets/news/$anews->photo", null, ['class'=>'width100']) !!}</div>
                         <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
+                            <div class="col-md-10 col-md-offset-1">
                                 <section class="text-content">
                                     {!! $anews->body !!}
                                 </section>
