482c6adaf248749db8ec4e134ce2c484fcc10b22
unknown <ncst.puje@gmail.com>
2016-07-26 12:27:45 +0900

    news changes

--- resources/views/pages/news_show.blade.php
+++ resources/views/pages/news_show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="placeholder100 row" style="margin-top: 50px;">
-        <div class="col-md-12 gheader">
+        <div class="col-md-12 col-lg-10 col-lg-offset-1 gheader">
             <header>
                 <h3>{{ $anews->title }}</h3>
 
@@ -12,7 +12,7 @@
     </div>
 <div class="row" id="projects" style="min-height: 500px">
     <div>
-            <div class="col-md-12" id="project_by_id">
+            <div class="col-md-12 col-lg-10 col-lg-offset-1" id="project_by_id">
                     <article>
                         <div>{!! Html::image("assets/news/$anews->photo", null, ['class'=>'width100']) !!}</div>
                         <div class="row">
