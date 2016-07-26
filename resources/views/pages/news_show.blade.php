d54c153c17c82da6e9637bf79bb678260e41083b
unknown <ncst.puje@gmail.com>
2016-07-26 12:22:07 +0900

    news changes

--- resources/views/pages/news_show.blade.php
+++ resources/views/pages/news_show.blade.php
@@ -11,8 +11,8 @@
         </div>
     </div>
 <div class="row" id="projects" style="min-height: 500px">
-    <div class="col-md-8 col-lg-6 col-lg-offset-2">
-            <div class="col-md-12" id="project_by_id">
+    <div>
+            <div class="col-md-12 col-lg-10 col-lg-offset-1" id="project_by_id">
                     <article>
                         <div>{!! Html::image("assets/news/$anews->photo", null, ['class'=>'width100']) !!}</div>
                         <div class="row">
