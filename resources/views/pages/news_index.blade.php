e70158f174b149966362d099592d87f6bce41720
unknown <ncst.puje@gmail.com>
2016-08-16 16:27:58 +0900

    helloo

--- resources/views/pages/news_index.blade.php
+++ resources/views/pages/news_index.blade.php
@@ -29,7 +29,7 @@
                             <article >
                                 <a href="{{ route('news_show_path', $anews->id) }}" >{!! Html::image("assets/news/thumbs/$anews->photo", null, ['class'=>'width100']) !!}</a>
                                 <figcaption>
-                                    <h4 class="news-title">{!! link_to_route('news_show_path', $anews->shortentitle(70), $anews->id)!!}</h4>
+                                    <h4 class="news-title">{!! link_to_route('news_show_path', $anews->shortentitle(50), $anews->id)!!}</h4>
 
                                     <div><p>{{ $anews->shorten(100)}} {!! link_to_route('news_show_path', 'Дэлгэрэнгүй ', $anews->id, ['class' => 'more'])!!}</p></div>
                                 </figcaption>
