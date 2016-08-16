0cfdb64aeddd9669b46939f3fbd9a6a888ca5d20
unknown <ncst.puje@gmail.com>
2016-08-16 21:10:17 +0900

    hurgelt

--- resources/views/layouts/partials/order.blade.php
+++ resources/views/layouts/partials/order.blade.php
@@ -1,5 +1,5 @@
 <div class="row">  
-  <div class="col-md-4 padding5"><a href="/content/buy" class="btn btn-primary btn-sm btn-block" role="button">Хүргэлтийн нөхцөл</a></div> 
+  <div class="col-md-4 padding5"><a href="/content/delivery" class="btn btn-primary btn-sm btn-block" role="button">Хүргэлтийн нөхцөл</a></div> 
   <div class="col-md-4 padding5"><a href="/content/return" class="btn btn-primary btn-sm btn-block" role="button">Буцаан олгох нөхцөл</a></div> 
   <div class="col-md-4 padding5"><a href="/content/buy" class="btn btn-primary btn-sm btn-block" role="button">Бараа захиалах заавар</a></div> 
 </div>
