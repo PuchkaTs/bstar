c816b525e059167e268d10d2bb0dc769bdc44c42
purevdorj tseepel <ncst.puje@gmail.com>
2016-08-24 18:59:06 +0900

    hloooo

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -108,7 +108,6 @@ class CardController extends Controller
 		<SessionID>".$order->sessionID."</SessionID>
 		</Request>
 		</TKKPG>";
-		dd($request);
 		$xml = $this->httpsPost("https://202.131.225.149:2233/Exec",($request),'name','password');
 		dd($xml);
 		return redirect($xml);
