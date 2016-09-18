d3d15f4a0e346bd0109c05f5ac62982be13dd6a4
purevdorj tseepel <ncst.puje@gmail.com>
2016-09-18 12:10:03 +0900

    hello

--- resources/views/layouts/partials/errors.blade.php
+++ resources/views/layouts/partials/errors.blade.php
@@ -1,6 +1,6 @@
 @if ($errors->any())
     <div class = "alert alert-danger">
-        <h3>Доорх хэсгийг бөглөнө үү!</h3>
+        <h3>Below fields are required!</h3>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
