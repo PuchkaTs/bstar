5a91d0fce355e4ea3eeb79879fa0055ac23e4574
purevdorj tseepel <ncst.puje@gmail.com>
2016-09-20 17:44:02 +0900

    buh delguuruud

--- resources/views/pages/store_index.blade.php
+++ resources/views/pages/store_index.blade.php
@@ -5,13 +5,21 @@
     <div class="simpleCart_shelfItem">
      
         <div class="row">
-        <div class="placeholder100">
+        <div class="placeholder100 hidden-xs">
             <div class="col-md-8 col-lg-6 col-md-offset-3">
                 <header>
                     <h3>Бүх дэлгүүрүүд</h3>
                 </header>
             </div>
-        </div>          
+        </div> 
+        <div class="visible-xs">
+            <div class="col-md-8 col-lg-6 col-md-offset-3">
+                <header>
+                    <h4 class="visible-xs">Бүх дэлгүүрүүд</h4>  
+                </header>
+            </div>
+        </div>         
+     
             <div class="col-md-3">
                 <section class="card">
                     <h5 class="item_name">Дэлгүүрийн нэрс:</h5>
@@ -25,33 +33,37 @@
 
             <div class="col-md-9">   
                 <div class="row">
-                        <div class="menu-list-container">
+                    <div class="col-md-12">
+                    <div class="menu-list-container">
                         @include('layouts.partials.topbanner')
 
                             <div class="menu-list">
-                            @foreach($companies->chunk(3) as $fourtype)
+                                <div class="menu-list">
                                 <div class="row">
-                                @foreach($fourtype as $company)
-                                <section class="col-md-4 col-sm-6">
-                                    <div class="product-card store-card">
-                                        <div class="company-logo">
-                                            <a href="{{ route('store_path', $company->url ) }}"><img src="/assets/stores/logo/{{$company->logo}}"></a></div>
-                                        <h3 class="store-title">{{$company->name}}</h3>
-                                        <h5>{!! link_to_route('store_path', $company->shorten(), $company->url)!!}</h5>
-                                    </div>
-                                </section>
-                                @endforeach
+                                @foreach($companies->chunk(3) as $fourtype)
+                                    @foreach($fourtype as $company)
+                                    <section class="col-md-4 col-sm-6">
+                                        <div class="product-card store-card">
+                                            <div class="company-logo">
+                                                <a href="{{ route('store_path', $company->url ) }}"><img src="/assets/stores/logo/{{$company->logo}}"></a></div>
+                                            <h4 class="store-title">{{$company->name}}</h4>
+                                            <h5>{!! link_to_route('store_path', $company->shorten(), $company->url)!!}</h5>
+                                        </div>                                            
+                                    </section>
+                                    @endforeach
+                                @endforeach                                    
                                 </div>
-                            @endforeach
+                                </div>      
                             </div>
-                            <div class="textcenter">
-                                {!! $companies->links() !!}         
-                            </div>                            
+                                <div class="textcenter">
+                                {!! $companies->links() !!}                                
+                                </div>                          
                             @include('layouts.partials.middlebanner')
                             @include('layouts.partials.reklam')
-                            @include('layouts.partials.bottombanner')                            
+                            @include('layouts.partials.bottombanner')                                
                         </div>
                     </div>
+                </div>
             </div>
         </div>
     </div>
@@ -62,19 +74,12 @@
 
 @section('script')
   <script>
-    $("#ex2").slider({});  
-
-    simpleCart({
-      checkout: {
-        type: "SendForm",
-        email: "you@yours.com",
-        url: "/checkout",
-        method: "GET" ,
-        success: "success"
-      },
-
-
-    });
-
+      simpleCart({
+        checkout: {
+          type: "SendForm",
+          email: "you@yours.com"
+        },
+        cartStyle : "div"
+      });
   </script>
 @stop
