eb5a3062697b46581f3ec73a85587e9b16792523
purevdorj tseepel <ncst.puje@gmail.com>
2016-09-21 17:20:26 +0900

    share

--- resources/views/pages/news_show.blade.php
+++ resources/views/pages/news_show.blade.php
@@ -21,6 +21,7 @@
                                 </section>
                             </div>
                         </div>
+                        <div class="fb-share-button" data-href="{{Request::url()}}" data-layout="button_count" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>                        
                     </article>
             </div>
     </div>
