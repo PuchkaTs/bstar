ad53f6bb076976a94a476dce587934af364f96eb
purevdorj tseepel <ncst.puje@gmail.com>
2016-09-21 18:16:05 +0900

    title

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts/default_min')
 
 @section('css')
-
+    <title>btitle</title>
+    <meta property="og:url" content="https://babystar.mn">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="title">
+    <meta property="og:site_name" content="Babystar.mn">
+    <meta property="og:description" content="сайтын зорилго нь">
+    <meta property="og:image" content="http://shiid.mn/assets/design/og_image_shiidmn.jpg">
 
 @stop
 
