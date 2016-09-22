c9c8fdf3e0bd1c7ac66ad7159c8ada89a240cd68
purevdorj tseepel <ncst.puje@gmail.com>
2016-09-22 17:01:29 +0900

    hello

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,85 +1,88 @@
 <footer class="footer">
     <div class="container-fluid">
-        <div class="colororange row">
-            <div class="col-md-2 col-md-offset-1 col-sm-3 col-xs-6">
-                <h4><i class="fa fa-users" aria-hidden="true"></i>Бидний тухай</h4>
-                <ul>
-                    <li>
-                        <a href="/content/about">Танилцуулга</a>
-                    </li>
-                    <li>
-                        <a href="/career">Ажлын байр</a>
-                    </li>
-                    <li>
-                        <a href="/content/advantage">Давуу тал</a>
-                    </li>
-                    <li>
-                        <a href="/form/cooperate">Хамтран ажиллах</a>
-                    </li>
-                    <li>
-                        <a href="/content/contact">Холбоо барих</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-2 col-sm-3 col-xs-6">
-                <h4><i class="fa fa-bell" aria-hidden="true"></i>Тусламж</h4>
-                <ul>
-                    <li>
-                        <a href="/content/buy">Худалдан авалт</a>
-                    </li>
-                    <li>
-                        <a href="/content/delivery">Хүргэлт</a>
-                    </li>
-                    <li>
-                        <a href="/content/return">Буцаалт</a>
-                    </li>
-                    <li>
-                        <a href="/content/payment-term">Төлбөрийн нөхцөл</a>
-                    </li>                    
-                    <li>
-                        <a href="/content/service-condition">Үйлчилгээний нөхцөл</a>
-                    </li>                
-                </ul>
-            </div> 
-            <div class="col-md-2 col-sm-3 col-xs-6">
-                <h4><i class="fa fa-eye" aria-hidden="true"></i>Хэрэглэгч</h4>
-                <ul>
-                    <li>
-                        <a href="/form/open-store">Дэлгүүр нээх</a>
-                    </li>                      
-                    <li>
-                        <a href="/content/confidentiality-policy">Нууцлалын бодлого</a>
-                    </li>                 
-                </ul>
-            </div>
-            <div class="col-md-2 col-sm-3 col-xs-6">
-                <h4><i class="fa fa-gift" aria-hidden="true"></i>Бэлэг</h4>
-                <ul>
-                    <li>
-                        <a href="/content/card">Бэлгийн карт</a>
-                    </li>
-                    <li>
-                        <a href="/content/ideas">Бэлэгний санаанууд</a>
-                    </li>
-                    <li>
-                        <a href="/content/discount-card">Хөнгөлөлтийн карт</a>
-                    </li>                      
-                    <li>
-                        <a href="/content/wishlist">Хүслийн жагсаалт</a>
-                    </li>                  
-                </ul>
-            </div>     
-            <div class="clearfix visible-xs-block visible-sm-block"></div>               
-            <div class="col-md-2 col-sm-3 social-container">
-                <h4><i class="fa fa-share-alt" aria-hidden="true"></i>Холбоо</h4>
-                <ul class="social ">
-                    <a href="https://www.facebook.com/babystar.mn.mongolia" target="_blank"><li class="flexcenter"><i class="fa fa-facebook-square" aria-hidden="true"></i></li></a>
-                    <a href="https://twitter.com/BabyStarMn" target="_blank"><li class="flexcenter"><i class="fa fa-twitter-square" aria-hidden="true"></i></li></a>
-                    <a href="https://www.instagram.com/babystar.mn/" target="_blank"><li class="flexcenter"><i class="fa fa-youtube-square" aria-hidden="true"></i></li></a>
-                    <a href="https://www.instagram.com/babystar.mn/" target="_blank"><li class="flexcenter"><i class="fa fa-instagram" aria-hidden="true"></i></li></a>
-                </ul>
-            </div> 
-        </div>        
+        <div class="col-md-12">
+            <div class="colororange row">
+                <div class="col-md-2 col-md-offset-1 col-sm-3 col-xs-6">
+                    <h4><i class="fa fa-users" aria-hidden="true"></i>Бидний тухай</h4>
+                    <ul>
+                        <li>
+                            <a href="/content/about">Танилцуулга</a>
+                        </li>
+                        <li>
+                            <a href="/career">Ажлын байр</a>
+                        </li>
+                        <li>
+                            <a href="/content/advantage">Давуу тал</a>
+                        </li>
+                        <li>
+                            <a href="/form/cooperate">Хамтран ажиллах</a>
+                        </li>
+                        <li>
+                            <a href="/content/contact">Холбоо барих</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-sm-3 col-xs-6">
+                    <h4><i class="fa fa-bell" aria-hidden="true"></i>Тусламж</h4>
+                    <ul>
+                        <li>
+                            <a href="/content/buy">Худалдан авалт</a>
+                        </li>
+                        <li>
+                            <a href="/content/delivery">Хүргэлт</a>
+                        </li>
+                        <li>
+                            <a href="/content/return">Буцаалт</a>
+                        </li>
+                        <li>
+                            <a href="/content/payment-term">Төлбөрийн нөхцөл</a>
+                        </li>                    
+                        <li>
+                            <a href="/content/service-condition">Үйлчилгээний нөхцөл</a>
+                        </li>                
+                    </ul>
+                </div> 
+                <div class="col-md-2 col-sm-3 col-xs-6">
+                    <h4><i class="fa fa-eye" aria-hidden="true"></i>Хэрэглэгч</h4>
+                    <ul>
+                        <li>
+                            <a href="/form/open-store">Дэлгүүр нээх</a>
+                        </li>                      
+                        <li>
+                            <a href="/content/confidentiality-policy">Нууцлалын бодлого</a>
+                        </li>                 
+                    </ul>
+                </div>
+                <div class="col-md-2 col-sm-3 col-xs-6">
+                    <h4><i class="fa fa-gift" aria-hidden="true"></i>Бэлэг</h4>
+                    <ul>
+                        <li>
+                            <a href="/content/card">Бэлгийн карт</a>
+                        </li>
+                        <li>
+                            <a href="/content/ideas">Бэлэгний санаанууд</a>
+                        </li>
+                        <li>
+                            <a href="/content/discount-card">Хөнгөлөлтийн карт</a>
+                        </li>                      
+                        <li>
+                            <a href="/content/wishlist">Хүслийн жагсаалт</a>
+                        </li>                  
+                    </ul>
+                </div>     
+                <div class="clearfix visible-xs-block visible-sm-block"></div>               
+                <div class="col-md-2 col-sm-3 social-container">
+                    <h4><i class="fa fa-share-alt" aria-hidden="true"></i>Холбоо</h4>
+                    <ul class="social ">
+                        <a href="https://www.facebook.com/babystar.mn.mongolia" target="_blank"><li class="flexcenter"><i class="fa fa-facebook-square" aria-hidden="true"></i></li></a>
+                        <a href="https://twitter.com/BabyStarMn" target="_blank"><li class="flexcenter"><i class="fa fa-twitter-square" aria-hidden="true"></i></li></a>
+                        <a href="https://www.instagram.com/babystar.mn/" target="_blank"><li class="flexcenter"><i class="fa fa-youtube-square" aria-hidden="true"></i></li></a>
+                        <a href="https://www.instagram.com/babystar.mn/" target="_blank"><li class="flexcenter"><i class="fa fa-instagram" aria-hidden="true"></i></li></a>
+                    </ul>
+                </div> 
+            </div>  
+        </div>
+
     </div>
 
     <div class="legalBanner flexcenter">
