9bc4b7ac1b69cebc9fbcc371c1e25485694c6146
PuchkaTs <ncst.puje@gmail.com>
2017-04-22 02:32:02 +0800

    test16

--- database/migrations/2017_04_21_202644_make_nullable_users_table.php
+++ database/migrations/2017_04_21_202644_make_nullable_users_table.php
@@ -12,12 +12,9 @@ class MakeNullableUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function ($table) {
-            $table->dropUnique('users_email_unique');
-        }); 
 
         Schema::table('users', function ($table) {
-            $table->integer('email')->nullable()->change();   
+            $table->string('email')->nullable()->change();   
         }); 
     }
 
