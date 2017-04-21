b856042fda650bdac655debc00954569b3507cae
PuchkaTs <ncst.puje@gmail.com>
2017-04-22 01:57:06 +0800

    test5

--- app/SocialAccountService.php
+++ app/SocialAccountService.php
@@ -23,7 +23,7 @@ class SocialAccountService
 
             $user = User::where('email', $providerUser->getEmail())->first();
 
-dd($providerUser->getEmail());
+
             if (!$user) {
 
                 $role = Role::whereName([
